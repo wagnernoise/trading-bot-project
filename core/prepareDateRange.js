28813d1dd3eb47d3c02ea1d0b5aef94a988b6b28
Wagner Rosa <50363193+wagnernoise@users.noreply.github.com>
2019-07-14 20:40:51 +0200

    Update code to ES6

--- core/prepareDateRange.js
+++ core/prepareDateRange.js
@@ -1,10 +1,10 @@
-var _ = require('lodash');
-var prompt = require('prompt-lite');
-var moment = require('moment');
+import { size, first, each } from 'lodash';
+import { get } from 'prompt-lite';
+import { unix } from 'moment';
 
-var util = require('./util');
-var config = util.getConfig();
-var dirs = util.dirs();
+import { getConfig, dirs as _dirs, setConfig, die } from './util';
+var config = getConfig();
+var dirs = _dirs();
 var log = require(dirs.core + 'log');
 
 var scan = require(dirs.tools + 'dateRangeScanner');
@@ -13,24 +13,24 @@ var scan = require(dirs.tools + 'dateRangeScanner');
 // daterange.
 var setDateRange = function(from, to) {
   config.backtest.daterange = {
-    from: moment.unix(from).utc().format(),
-    to: moment.unix(to).utc().format(),
+    from: unix(from).utc().format(),
+    to: unix(to).utc().format(),
   };
-  util.setConfig(config);
+  setConfig(config);
 }
 
 
-module.exports = function(done) {
+export default function(done) {
   scan((err, ranges) => {
 
-    if(_.size(ranges) === 0)
-      util.die('No history found for this market', true);
+    if(size(ranges) === 0)
+      die('No history found for this market', true);
 
-    if(_.size(ranges) === 1) {
-      var r = _.first(ranges);
+    if(size(ranges) === 1) {
+      var r = first(ranges);
       log.info('Gekko was able to find a single daterange in the locally stored history:');
-      log.info('\t', 'from:', moment.unix(r.from).utc().format('YYYY-MM-DD HH:mm:ss'));
-      log.info('\t', 'to:', moment.unix(r.to).utc().format('YYYY-MM-DD HH:mm:ss'));
+      log.info('\t', 'from:', unix(r.from).utc().format('YYYY-MM-DD HH:mm:ss'));
+      log.info('\t', 'to:', unix(r.to).utc().format('YYYY-MM-DD HH:mm:ss'));
 
       
       setDateRange(r.from, r.to);
@@ -42,22 +42,22 @@ module.exports = function(done) {
       'Please pick the daterange you are interested in testing:'
     );
 
-    _.each(ranges, (range, i) => {
+    each(ranges, (range, i) => {
       log.info('\t\t', `OPTION ${i + 1}:`);
-      log.info('\t', 'from:', moment.unix(range.from).utc().format('YYYY-MM-DD HH:mm:ss'));
-      log.info('\t', 'to:', moment.unix(range.to).utc().format('YYYY-MM-DD HH:mm:ss'));
+      log.info('\t', 'from:', unix(range.from).utc().format('YYYY-MM-DD HH:mm:ss'));
+      log.info('\t', 'to:', unix(range.to).utc().format('YYYY-MM-DD HH:mm:ss'));
     });
 
-    prompt.get({name: 'option'}, (err, result) => {
+    get({name: 'option'}, (err, result) => {
 
       var option = parseInt(result.option);
       if(option === NaN)
-        util.die('Not an option..', true);
+        die('Not an option..', true);
 
       var range = ranges[option - 1];
 
       if(!range)
-        util.die('Not an option..', true);
+        die('Not an option..', true);
 
       setDateRange(range.from, range.to);
       return done();
